d11b8562ac6249c93c01f76a03d1153b8f3da543
BazylevDV <dmbazylev@gmail.com>
2024-11-02 23:00:19 +0500

    Update module13_4.py

--- module13_4.py
+++ module13_4.py
@@ -21,27 +21,27 @@ if __name__ == '__main__':
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
 
-    # Функция для подтверждения желания рассчитать норму калорий
+    
     @dp.message(Command("start"))
     async def ask_confirm(message: types.Message, state: FSMContext):
         await message.answer("Введите свой возраст:")
         await state.set_state(UserState.age)
 
-    # Функция для установки возраста
+    
     @dp.message(StateFilter(UserState.age))
     async def set_age(message: types.Message, state: FSMContext):
         await state.update_data(age=int(message.text))
         await message.answer("Введите свой рост в сантиметрах:")
         await state.set_state(UserState.growth)
 
-    # Функция для установки роста
+    
     @dp.message(StateFilter(UserState.growth))
     async def set_growth(message: types.Message, state: FSMContext):
         await state.update_data(growth=int(message.text))
         await message.answer("Введите свой вес в килограммах:")
         await state.set_state(UserState.weight)
 
-    # Функция для установки веса
+    
     @dp.message(StateFilter(UserState.weight))
     async def set_weight(message: types.Message, state: FSMContext):
         await state.update_data(weight=int(message.text))
